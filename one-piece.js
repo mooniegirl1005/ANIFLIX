5cf8e12f4484b5c486ef654287d065940a028e00
Sk Jasina <jassikutti10@gmail.com>
2025-07-03 20:40:02 +0530

    Add files via upload

---
+++ one-piece.js
@@ -0,0 +1,8 @@
+const animeData = {
+    "one-piece": {
+      title: "One Piece - Episode 1",
+      video: "C:\Users\skjas\Downloads\SPY x FAMILY _ Official Trailer _ Netflix.mp4",
+      poster: "images/one-piece-thumb.jpg"
+    }
+  };
+  
